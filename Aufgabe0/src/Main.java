8c9130c3955fb4793e58c533f4f21bc83e4632fb
nwienand <niklas.wienand@gmx.de>
2024-10-15 20:07:00 +0200

    Aufgabe 0 angelegt

--- Aufgabe0/src/Main.java
+++ Aufgabe0/src/Main.java
@@ -1,4 +1,8 @@
-public class Aufgabe0 {
+/**
+ * @author Niklas Wienand (6070531)
+ */
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
